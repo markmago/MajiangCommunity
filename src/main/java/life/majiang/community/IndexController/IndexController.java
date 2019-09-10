fee405995173cb1d26ccaccda6845e5aae49b703
hustmark <hustmark@hust.edu.cn>
2019-09-10 23:48:31 +0800

    导航条

---
+++ src/main/java/life/majiang/community/IndexController/IndexController.java
@@ -0,0 +1,16 @@
+package life.majiang.community.IndexController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+@author mark
+@time 2019/9/9
+*/
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String Index() {
+        return "index";
+    }
+}
